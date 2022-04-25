aec67c1c3291282af17f147bafad1f30a7308cac
Maya Krishnamoorthy <mkrishn@email.unc.edu>
2022-04-25 18:48:00 -0400

    middleware

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
-// MIDDLEWARE CODE API
+// MIDDLEWARE CODE
 app.use( (req, res, next) => {
     let logdata = {
         remoteaddr: req.ip,
